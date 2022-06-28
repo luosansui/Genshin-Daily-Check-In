01976054fb1aa645cbba26c7c4d853bd252ac3a8
洛三岁 <58138234+luosansui@users.noreply.github.com>
2022-06-29 01:27:35 +0800

    2022-6-29
    Add error retry function

--- src/app.js
+++ src/app.js
@@ -10,27 +10,32 @@ const headers = {
     'referer': `${origin}/`,
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36'
 }
-//创建日志目录
-tool.mkLog()
-//签到功能
-const signIn = ()=> axios({
+//发送请求
+const signIn = () => axios({
     method: 'post',
     headers,
     url:`${url}?lang=${cookie.mi18nLang}`,
     data: { act_id }
 })
-//定时签到
-const job = new CronJob(
-	'30 0 0 * * *',
-	async function() {
-        try {
-            const { data:{ message } } = await signIn()
-            if(message === 'OK')tool.log('签到成功')
-            else tool.log(message)
-        } catch (error) {
-            tool.log(error.message)
+//签到功能
+const checkIn = async (count = 0) => {
+    try {
+        const { data:{ message } } = await signIn()
+        if(message === 'OK')tool.log('签到成功')
+        else tool.log(message)
+    } catch (error) {
+        if(count >= 5){
+            tool.log(`${count}次重试后仍签到失败,已停止今日签到任务`)
+            return
         }
-	}
-)
+        tool.log(`出现错误: ${error.message}, 30秒后开始第${count + 1}次重试`)
+        await new Promise(res => setTimeout(() => res(),30000))
+        checkIn(count + 1)
+    }
+}
+//定时签到
+const job = new CronJob(`${Math.floor(Math.random() * 59)} 0 0 * * *`,checkIn)
+//创建日志目录
+tool.mkLog()
 //开始作业
 job.start()
